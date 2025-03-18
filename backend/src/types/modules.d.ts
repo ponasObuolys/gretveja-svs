1ceff4e9bf8a89b46f5084296b980bb8dbc86399
Aurimas Butvilauskas <67857119+ponasObuolys@users.noreply.github.com>
2025-03-18 13:00:19 +0200

    Backend pritaikymas

---
+++ backend/src/types/modules.d.ts
@@ -0,0 +1,6 @@
+// Declaration file for modules without TypeScript definitions
+
+declare module '@supabase/supabase-js';
+declare module 'csv-stringify';
+declare module 'exceljs';
+declare module 'csv-parse/sync';
